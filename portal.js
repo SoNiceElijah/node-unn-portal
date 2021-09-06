caec110f672d7a5a650577f65b32d77b11e43a72
elijah <soniceelijah@yandex.ru>
2021-09-07 00:37:04 +0300

    naive new features

--- portal.js
+++ portal.js
@@ -4,9 +4,17 @@ const schedule = require('./lib/schedule');
 const search = require('./lib/search');
 const timetable = require('./lib/timetable');
 
+const { on, off } = require('./lib/modifier'); 
+
 module.exports = {
+    
     base,
+
+    modifyon : on,
+    modifyoff : off,
+
     schedule,
     search,
     timetable
+    
 };
